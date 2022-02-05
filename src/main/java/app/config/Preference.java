afcc27fbca37a82867f864a19b8b6eb248dca39a
fly2moon2 <fly2moon2@yahoo.com>
2022-02-05 22:21:47 +0800

    akprefscope

--- src/main/java/app/config/Preference.java
+++ src/main/java/app/config/Preference.java
@@ -4,13 +4,21 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 
 
 @Entity
 @Table (name="akpref")
 public class Preference extends PanacheEntity {
-    public PreferenceScope scope;
+    @ElementCollection
+    @CollectionTable(name="akprefscope")
+    public Set<PreferenceScope> scope=new HashSet<>();
     public Long subjectId;
     @Column(nullable=false, length = 5)
     public String code;
--- src/main/java/app/config/PreferenceScope.java
+++ src/main/java/app/config/PreferenceScope.java
@@ -2,23 +2,27 @@ package app.config;
 
 import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Parent;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
-@Entity
-@Table (name="akprefscope")
-public class PreferenceScope extends PanacheEntity {
+
+@Embeddable
+public class PreferenceScope {
     @Column(nullable=false, length = 5)
     public String code;
     @Column(nullable=false, length = 30)
     public String prefscope;
 
     @JsonbTransient
-    @ManyToOne(fetch = FetchType.LAZY)
+    // @ManyToOne(fetch = FetchType.LAZY)
+    @Parent
     public Preference pref;
 /*
 @Override
